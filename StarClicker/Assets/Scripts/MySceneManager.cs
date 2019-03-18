082a5727ff32fefdec3aa11a63c38e361fec66ba
ejp4604 <ejp4604@g.rit.edu>
2019-03-18 15:12:24 -0400

    Basis for MVI: Placeholder star sprites (normal and bad). Spawns between 0 and 3 stars and chooses between either star type. Spawns them within bounds of canvas which is bound to the same dimensions as the camera. MySceneManager object/script that holds score and prefabs for use in game.

---
+++ StarClicker/Assets/Scripts/MySceneManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MySceneManager : Singleton<MySceneManager>
+{
+    // values for determining score
+    float score;
+    float multiplier;
+    int stars;
+
+    // prefabs for use during game
+    public GameObject starNormalPrefab;
+    public GameObject starBadPrefab;
+}
---
+++ StarClicker/Assets/Scripts/SpawnStars.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnStars : MonoBehaviour
+{
+    // half width/height of camera/canvas
+    float halfHeight;
+    float halfWidth;
+
+    // list that holds stars
+    List<GameObject> maxStars;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // sets variables to half height and width of camera/canvas
+        halfHeight = Camera.main.orthographicSize;
+        halfWidth = halfHeight * Camera.main.aspect;
+
+        // instantiates list for use
+        maxStars = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // for loop that picks a random number of stars to spawn at once
+        // spawns them within the bounds of the camera/canvas
+        // **in the future, multiple stars will cross the screen at once
+        // **in the future, stars will spawn outside of camera/canvas bounds
+        for(int x = 0; x < Random.Range(0,4); x++)
+            // stops at 50 stars
+            if(maxStars.Count < 51)
+                // instantiates stars and adds them to a list
+                if(Random.Range(0,2) == 0)
+                    maxStars.Add(Instantiate(MySceneManager.Instance.starNormalPrefab, new Vector2(Random.Range(-halfWidth, halfWidth), Random.Range(-halfHeight, halfHeight)), Quaternion.identity));
+                else
+                    maxStars.Add(Instantiate(MySceneManager.Instance.starBadPrefab, new Vector2(Random.Range(-halfWidth, halfWidth), Random.Range(-halfHeight, halfHeight)), Quaternion.identity));
+    }
+}
